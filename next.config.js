b5a8cc07b0b550ca5ce22dd7ccee1b44cdf9e6e4
mathiasfc <mathias.falci@gmail.com>
2024-02-20 20:39:54 -0300

    :wrench: added env vars to next config file

--- next.config.js
+++ next.config.js
@@ -5,6 +5,11 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
-}
+  env: {
+    SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
+    EMAIL_SENDER: process.env.EMAIL_SENDER,
+    EMAIL_RECEIVER: process.env.EMAIL_RECEIVER,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
